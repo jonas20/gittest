62b511843d2ef858dbddff3946fd2111435df7ae
jonas <alikeke@gmail.com>
2013-09-06 11:02:27 +0800

    Initial Commit

---
+++ gittest2/GTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GTAppDelegate.h
+//  gittest2
+//
+//  Created by jonas hu on 13-9-6.
+//  Copyright (c) 2013年 jonas hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gittest2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gittest2
+//
+//  Created by jonas hu on 13-9-6.
+//  Copyright (c) 2013年 jonas hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GTAppDelegate class]));
+    }
+}
