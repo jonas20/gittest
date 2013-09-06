4e68da023ebe435d92ef2e87566298b51072a424
jonas <alikeke@gmail.com>
2013-09-06 14:12:25 +0800

    344

--- gittest2/GTAppDelegate.m
+++ gittest2/GTAppDelegate.m
@@ -16,18 +16,10 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
-    
-    
-    
-    
-    
-    
-    
-    
-    ///test
-    
-    
-    
+    /***
+     jonas
+     
+     *////
     
     //jonas
     return YES;
